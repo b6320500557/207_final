96b718dc6266ee64e8864efea166aab71a396a85
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-29 19:45:52 +0700

    add 8

--- looblike - 6320500557.c
+++ looblike - 6320500557.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int n,i,j,k=0;;
+    int n,i,j,k=0;
     scanf("%d",&n);
-    int x[n],m[n],temp[n];;
+    int x[n],m[n];
     for(i=1; i<=n; i++)
     {
         scanf("%d",&x[i]);
@@ -14,12 +14,9 @@ int main()
     }
      for(i=1; i<=n; i++)
     {
-        if(x[i] > m[i])
+        if(x[i++] == x[i+1])
         {
-             x[i] = m[i];
-        }
-        if(x[i] == x[i+1])
-        {
+            m[i] = x[i];
             printf("%d ",x[i]);
         }
     }
