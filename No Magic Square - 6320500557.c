a2f8d8513dbd9a131b82fa681c48b1731f707b7c
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-29 18:56:48 +0700

    add 1

--- No Magic Square - 6320500557.c
+++ No Magic Square - 6320500557.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2,n3,i,j,k;
+    scanf("%d %d %d",&n1,&n2,&n3);
+    int x[n1][n1],y[n2][n2],z[n3][n3],sum[20];
+    for(i=0; i<n1; i++)
+    {
+        for(j=0; j<n1; j++)
+        {
+            scanf("%d",&x[i][j]);
+        }
+    }
+  /*  for(i=0; i<n2; i++)
+    {
+        for(j=0; j<n2; j++)
+        {
+            scanf("%d",&y[i][j]);
+        }
+    }
+    for(i=0; i<n3; i++)
+    {
+        for(j=0; j<n3; j++)
+        {
+            scanf("%d",&z[i][j]);
+        }
+    }*/
+     for(i=0; i<n1; i++)
+    {
+        sum[i] += x[i];
+        printf("%d ",sum[i]);
+    }
+
+
+    return 0;
+}
 
