41ad67ad81250661a08f9cf122a4a6aaf6494aa7
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-29 18:10:47 +0700

    add 4

--- looblike - 6320500557.c
+++ looblike - 6320500557.c
@@ -3,26 +3,24 @@ int main()
 {
     int n,i,j,k=0;
     scanf("%d",&n);
-    int x[n];
+    int x[n],m[n];
     for(i=1; i<=n; i++)
     {
         scanf("%d",&x[i]);
+    }
+    for(i=1; i<=n; i++)
+    {
+        m[i] = 0;
     }
      for(i=1; i<=n; i++)
     {
         if(x[i] == x[i+1])
         {
-            if(x[i] == x[i++])
-            {
-                printf("%d ",x[i]);
-            }
-            else
-            {
-                printf("%d ",x[i+1]);
-            }
+            m[i] = x[i];
+           printf("%d",m[i]);
         }
-
     }
+
 
 
     return 0;
